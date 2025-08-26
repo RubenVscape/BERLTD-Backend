623cbc51af1ad98c24a937b925c16c1ad11b63a7
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-26 16:28:01 -0600

    changes 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,6 @@
-import app from "..";
-export default app;
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '..'; // your Express app export
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res); // let Express handle it
+};
