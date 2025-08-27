0bf27462eb88e94da511f23c99c2b462ac367c44
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-26 22:33:30 -0600

    push new

--- dist/api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("../index"));
-const serverless_express_1 = __importDefault(require("@vendia/serverless-express"));
-// Vercel needs a function handler, not just an express app
-exports.default = (0, serverless_express_1.default)({ app: index_1.default });
