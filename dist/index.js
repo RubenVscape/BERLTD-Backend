a452929ca713bef975405b15d8f0587315de8f79
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-26 22:53:30 -0600

    asdadsadhasashd

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
 const db_1 = require("./config/db");
 const authorizationChecker_1 = require("./auth/authorizationChecker");
+const path_1 = __importDefault(require("path"));
 require("dotenv").config();
+const controllersPath = process.env.NODE_ENV === "production"
+    ? path_1.default.join(__dirname, "controller/*.js")
+    : path_1.default.join(__dirname, "controller/*.ts");
+console.log(controllersPath);
 const routingControllersOptions = {
     routePrefix: "/api",
-    controllers: [`${__dirname}/controller/*.controller.*`],
+    controllers: [`${controllersPath}/controller/*.controller.*`],
     validation: true,
     classTransformer: true,
     cors: true,
