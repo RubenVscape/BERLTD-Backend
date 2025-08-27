8e178c75f595a533465a2844b0da6ecdb37ee2f3
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-26 22:29:21 -0600

    pslsls

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,3 @@
-import app from "../index";
-import serverless from "@vendia/serverless-express";
-
-// Vercel needs a function handler, not just an express app
-export default serverless({ app });
+import app from "..";
+export default app;
 
