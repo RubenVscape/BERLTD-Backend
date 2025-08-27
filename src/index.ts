2752ca101b4aca606e0cc2b7d04ab9f58c9e47d2
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-26 22:07:38 -0600

    new changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import express from 'express';
-import app from './main';
+import 'reflect-metadata';
+import { createExpressServer, RoutingControllersOptions } from 'routing-controllers';
+import { connectDB } from './config/db';
+import { authorizationChecker, currentUserChecker } from './auth/authorizationChecker';
+require("dotenv").config();
 
+const routingControllersOptions: RoutingControllersOptions = {
+  routePrefix: "/api",
+  controllers: [`${__dirname}/controller/*.controller.*`],
+  validation: true,
+  classTransformer: true,
+  cors: true,
+  defaultErrorHandler: false,
+  authorizationChecker,
+  currentUserChecker
+};
 
-const server = express();
+connectDB();
 
-server.use(app);
+const app = createExpressServer(routingControllersOptions);
 
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3001, () => {
+    console.log("[Server] running at http://localhost:3001");
+  });
+}
 
-export default app;
+export default app; 
