818dd6e9ec707abf2e89ec4f79783249957e9ddb
Ruben Barrienentos <rubenbarrienentos@Rubens-MacBook-Pro.local>
2025-08-27 08:18:34 -0600

    remove api

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "../index";
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-// Vercel expects a function with (req, res)
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
